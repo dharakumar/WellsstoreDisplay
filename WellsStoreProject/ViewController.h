bcf95c1f95a0ce51b13171faa7d49b246034e436
Admin <admin@admins-macbook-pro-3.local>
2017-01-12 11:46:23 -0800

    Initial Commit

---
+++ WellsStoreProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WellsStoreProject
+//
+//  Created by Admin on 1/12/17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
